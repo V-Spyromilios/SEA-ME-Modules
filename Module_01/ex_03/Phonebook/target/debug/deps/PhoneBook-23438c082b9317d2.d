9d970f77bdcb7984c5e0336163307e58d992c47b
Evangelos Spyromilios <vsp84@icloud.com>
2025-07-04 20:04:41 +0200

    tidy up

--- Module_01/ex_03/Phonebook/target/debug/deps/PhoneBook-23438c082b9317d2.d
+++
@@ -1,7 +0,0 @@
-/Users/evangelos/Documents/SEA-ME_Projects/SEA-ME-Modules/Module_01/ex_03/Phonebook/target/debug/deps/libPhoneBook-23438c082b9317d2.rmeta: src/main.rs src/contact.rs src/phonebook.rs
-
-/Users/evangelos/Documents/SEA-ME_Projects/SEA-ME-Modules/Module_01/ex_03/Phonebook/target/debug/deps/PhoneBook-23438c082b9317d2.d: src/main.rs src/contact.rs src/phonebook.rs
-
-src/main.rs:
-src/contact.rs:
-src/phonebook.rs:
