d08f84c5c02ea74cc93a425db75aa87e117fa42b
Evangelos Spyromilios <vsp84@icloud.com>
2025-07-04 20:44:58 +0200

    Clean up ignored files and apply global .gitignore

--- Module_01/ex_03/Phonebook/target/debug/deps/PhoneBook-b226db3fa5322556.d
+++
@@ -1,7 +0,0 @@
-/Users/evangelos/Documents/SEA-ME_Projects/SEA-ME-Modules/Module_01/ex_03/Phonebook/target/debug/deps/libPhoneBook-b226db3fa5322556.rmeta: src/main.rs src/contact.rs src/phonebook.rs
-
-/Users/evangelos/Documents/SEA-ME_Projects/SEA-ME-Modules/Module_01/ex_03/Phonebook/target/debug/deps/PhoneBook-b226db3fa5322556.d: src/main.rs src/contact.rs src/phonebook.rs
-
-src/main.rs:
-src/contact.rs:
-src/phonebook.rs:
--- Module_01/ex_03/Phonebook/target/debug/deps/figlet_rs-7d103103e2ab2c1f.d
+++
@@ -1,6 +0,0 @@
-/Users/evangelos/Documents/SEA-ME_Projects/SEA-ME-Modules/Module_01/ex_03/Phonebook/target/debug/deps/libfiglet_rs-7d103103e2ab2c1f.rmeta: /Users/evangelos/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/figlet-rs-0.1.5/src/lib.rs /Users/evangelos/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/figlet-rs-0.1.5/src/standard.flf
-
-/Users/evangelos/Documents/SEA-ME_Projects/SEA-ME-Modules/Module_01/ex_03/Phonebook/target/debug/deps/figlet_rs-7d103103e2ab2c1f.d: /Users/evangelos/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/figlet-rs-0.1.5/src/lib.rs /Users/evangelos/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/figlet-rs-0.1.5/src/standard.flf
-
-/Users/evangelos/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/figlet-rs-0.1.5/src/lib.rs:
-/Users/evangelos/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/figlet-rs-0.1.5/src/standard.flf:
